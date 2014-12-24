1715e36c676aabe0ab168b60a3e51bede8c9627c
Luis Felipe Boscan Nava <luife642@gmail.com>
2014-12-24 19:17:51 +0000

    Testing version_compare() in myFirstPlugin.php

--- wp-content/plugins/myFirstPlugin.php
+++ wp-content/plugins/myFirstPlugin.php
@@ -1,5 +1,5 @@
 <?php
-// All it's for test!
+//= All it's for test!
 /*
 Plugin Name: First Plugin
 Plugin URI: www.lfboself.site88.net 
@@ -8,3 +8,8 @@ Author: Luis Boscan
 Version: 1.0
 Author URI: www.lfboself.site88.net
 */
+
+global $wp_version; // Para obtener la version
+if(!version_compare($wp_version, "3.0", ">=")) {
+    die("Necesitas una version posterior para este plugin");
+}
