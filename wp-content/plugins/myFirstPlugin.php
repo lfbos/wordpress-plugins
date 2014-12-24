3d3c6c8dee20f982fc2352f30b05f81184925f78
Luis Felipe Boscan Nava <luife642@gmail.com>
2014-12-24 19:00:16 +0000

    Testing the first plugin in WordPress

---
+++ wp-content/plugins/myFirstPlugin.php
@@ -0,0 +1,10 @@
+<?php
+// All it's for test!
+/*
+Plugin Name: First Plugin
+Plugin URI: www.lfboself.site88.net 
+Description: Plugin Description
+Author: Luis Boscan
+Version: 1.0
+Author URI: www.lfboself.site88.net
+*/
