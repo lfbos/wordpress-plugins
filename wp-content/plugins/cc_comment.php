f008331d75400b82718634c85c079b13e5cf0abf
Luis Felipe Boscan Nava <luife642@gmail.com>
2014-12-24 20:07:36 +0000

    New plugin that will send you an email when someone posts a comment

---
+++ wp-content/plugins/cc_comment.php
@@ -0,0 +1,23 @@
+<?php
+//= All it's for test!
+/*
+Plugin Name: CC Comment
+Plugin URI: www.lfboself.site88.net 
+Description: This plugin will send an email
+Author: Luis Boscan
+Version: 1.0
+Author URI: www.lfboself.site88.net
+*/
+
+function cc_comment() {
+    global $_REQUEST;
+    
+    // Details
+    $to = "luife642@gmail.com";
+    $subject = "Nuevo comentario en el blog" . $_REQUEST['subject'];
+    $mensaje = "Mensaje de: ".$_REQUEST['name']." por email " . $_REQUEST['email']."\n".$_REQUEST['comments'];
+    
+    mail($to,$subject,$mensaje);
+}
+
+add_action('comment_post','cc_comment');
